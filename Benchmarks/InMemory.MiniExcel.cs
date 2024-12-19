4b37433204ac71ea7ecae6f302bb710a98d359a4
TinyXlsx <tinyxlsx@proton.me>
2024-12-19 01:57:11 +0100

    Adjust MiniExcel benchmark as it wasn't resulting in the correct file

--- Benchmarks/InMemory.MiniExcel.cs
+++ Benchmarks/InMemory.MiniExcel.cs
@@ -1,7 +1,6 @@
 ﻿using BenchmarkDotNet.Attributes;
 using MiniExcelLibs;
 using MiniExcelLibs.Attributes;
-using MiniExcelLibs.OpenXml;
 
 namespace Benchmarks;
 
@@ -12,43 +11,59 @@ public partial class InMemory
     {
         using var stream = new MemoryStream();
 
-        var configuration = new OpenXmlConfiguration
-        {
-            DynamicColumns = new DynamicExcelColumn[] {
-                new("Column1"),
-                new("Column2"),
-                new("Column3"),
-                new("Column4"),
-                new("Column5") { Format="yyyy-MM-dd" },
-                new("Column6") { Format="yyyy/MM/dd" },
-                new("Column7"),
-                new("Column8") { Format="0.00" },
-                new("Column9") { Format="0.00%" },
-                new("Column10") { Format="0.00E+00" },
-                new("Column11") { Format="$#,##0.00" },
-                new("Column12") { Format="#,##0.00 [$USD]" },
-            },
-        };
-
         var values = Enumerable
             .Range(1, Records)
             .Select(item =>
-                new object[]
+                new Dto
                 {
-                    new { Column1 = false },
-                    new { Column2 = 123456 },
-                    new { Column3 = 123.456m },
-                    new { Column4 = 123.456 },
-                    new { Column5 = DateTime.Now },
-                    new { Column6 = DateTime.Now },
-                    new { Column7 = "Text" },
-                    new { Column8 = 123.456 },
-                    new { Column9 = 123.456 },
-                    new { Column10 = 123.456 },
-                    new { Column11 = 123.456 },
-                    new { Column12 = 123.456 },
+                    Column1 = false,
+                    Column2 = 123456,
+                    Column3 = 123.456m,
+                    Column4 = 123.456,
+                    Column5 = DateTime.Now,
+                    Column6 = DateTime.Now,
+                    Column7 = "Text",
+                    Column8 = 123.456,
+                    Column9 = 123.456,
+                    Column10 = 123.456,
+                    Column11 = 123.456,
+                    Column12 = 123.456,
                 });
 
-        stream.SaveAs(value: values, configuration: configuration, excelType: ExcelType.XLSX);
+        stream.SaveAs(value: values, excelType: ExcelType.XLSX);
+    }
+
+    public class Dto
+    {
+        public bool Column1 { get; set; }
+
+        public int Column2 { get; set; }
+
+        public decimal Column3 { get; set; }
+
+        public double Column4 { get; set; }
+
+        [ExcelFormat("yyyy-MM-dd")]
+        public DateTime Column5 { get; set; }
+
+        [ExcelFormat("yyyy/MM/dd")]
+        public DateTime Column6 { get; set; }
+
+        public string Column7 { get; set; }
+
+        [ExcelFormat("0.00")]
+        public double Column8 { get; set; }
+
+        [ExcelFormat("0.00%")]
+        public double Column9 { get; set; }
+
+        [ExcelFormat("0.00E+00")]
+        public double Column10 { get; set; }
+
+        [ExcelFormat("$#,##0.00")]
+        public double Column11 { get; set; }
+
+        [ExcelFormat("#,##0.00 [$USD]")]
+        public double Column12 { get; set; }
     }
 }
